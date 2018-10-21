223e06d3857f40db58526652b236ecefc114a8ab
Matt <simonmatt1985@gmail.com>
2018-10-21 14:46:17 +0800

    fixed the issue: vue-loader was used without the corresponding plugin. Make sure to include VueLoaderPlugin in your webpack config

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,5 @@
 /// <reference path="globals/axios/index.d.ts" />
+/// <reference path="globals/moment/index.d.ts" />
+/// <reference path="globals/mongoose/index.d.ts" />
+/// <reference path="globals/socket.io-client/index.d.ts" />
+/// <reference path="globals/socket.io/index.d.ts" />
